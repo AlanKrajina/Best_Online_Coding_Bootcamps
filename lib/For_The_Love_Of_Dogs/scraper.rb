397e79e948f040038f29df8d8b5758ab727b62fe
Alan Krajina <alankrajina88@gmail.com>
2019-06-14 15:07:53 +0000

    update scraper

--- lib/For_The_Love_Of_Dogs/scraper.rb
+++ lib/For_The_Love_Of_Dogs/scraper.rb
@@ -6,17 +6,20 @@ class ForTheLoveOfDogs::Scraper
   def self.scrape_index_page
     html = open("https://www.battersea.org.uk/dogs/dog-rehoming-gallery")
     doc = Nokogiri::HTML(html)
-    binding.pry
     dogs = []
 
-#    doc.css(".student-card").each do |student|
-#      student_index_array << {
-#        :name => student.css("h4.student-name").text,
-#        :location => student.css("p.student-location").text,
-#        :profile_url => student.css("a").attribute("href").value
-#      }
-#    end
-#    dogs
+    doc.css(".listing_container").each do |dog|
+      dogs << {
+        :name => dog.css(".animal_name a.ng-binding").text,
+        
+        :age => dog.css(".animal_name a.ng-binding").text,
+        :centre => dog.css(".animal_name a.ng-binding").text
+        
+      }
+    end
+    binding.pry
+    
+    dogs
   end
 
 #  def self.scrape_bootcamp_page(bootcamp_url)
