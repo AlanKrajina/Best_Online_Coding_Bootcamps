ee31268855fb662859a4b337eea3308eebf3ddb3
Alan Krajina <alankrajina88@gmail.com>
2019-06-16 09:47:01 +0000

    update

--- lib/Best_Online_Coding_Bootcamps/scraper.rb
+++ lib/Best_Online_Coding_Bootcamps/scraper.rb
@@ -15,11 +15,17 @@ class BestOnlineCodingBootcamps::Scraper
         bootcamp.rating = camp.css("div.ranking-item__header--left span.ranking-item__rating--value").text.split.join(' ')
         bootcamp.subjects = camp.css("table tr:nth-child(3) td").text.split.join(' ')
         bootcamp.url = camp.css("div.ranking-item__header h3 a").attribute("href").value 
-        bootcamp.save
+        BestOnlineCodingBootcamps::Bootcamps.new(name, rating, subjects, url)
     end
   end     
+  
 
-
+  def self.scrape_details(bootcamps)
+    doc = Nokogiri::HTML(open("https://www.switchup.org/rankings/best-online-bootcamps#{bootcamps.url}"))
+    doc.css("article.copy p.litNoteText").each do |paragraph|
+      topic.content << paragraph.text.strip
+    end
+  end
  
 
 
