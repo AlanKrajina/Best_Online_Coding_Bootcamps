ad669e3740e3188d34ffff5157ec50023d55ea7d
Alan Krajina <alankrajina88@gmail.com>
2019-06-16 19:14:47 +0000

    update scraper

--- lib/Best_Online_Coding_Bootcamps/scraper.rb
+++ lib/Best_Online_Coding_Bootcamps/scraper.rb
@@ -5,54 +5,31 @@ require "pry"
 class BestOnlineCodingBootcamps::Scraper
 
   def self.scrape_index_page
-    html = open("https://www.switchup.org/bootcamps/coding-dojo")
+    html = open("https://www.switchup.org/rankings/best-online-bootcamps")
     doc = Nokogiri::HTML(html)
     
-        about = doc.css("h2.topic-title").text.split.join(' ')
-        website = doc.css("div.ranking-item a").attribute("href").value 
-        
-        if doc.css("div.span12 blockquote.topic-text p").text.include? "..." then info = doc.css("div.span12 blockquote.topic-text p a").attribute("onclick").value[35..-25].delete! '\\<>/' elsif !doc.css("div.span12 blockquote.topic-text p").text.include? "..." then info = doc.css("div.span12 blockquote.topic-text p").text.chomp end
-        
-        
-        
-        if doc.css("div.extra-info p:nth-child(3) span").text.include? "..." then scholarships = doc.css("div.extra-info p:nth-child(3) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(3) span").text.include? "..." then scholarships = doc.css("div.extra-info p:nth-child(3) span").text.split.join(' ') end
-        
-        if doc.css("div.extra-info p:nth-child(2) span").text.include? "..." then programs = doc.css("div.extra-info p:nth-child(2) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(2) span").text.include? "..." then programs = doc.css("div.extra-info p:nth-child(2) span").text.split.join(' ') end
-        
-        if doc.css("div.extra-info p:nth-child(1) span").text.include? "..." then locations = doc.css("div.extra-info p:nth-child(1) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(1) span").text.include? "..." then locations = doc.css("div.extra-info p:nth-child(1) span").text.split.join(' ') end
-  
-     binding.pry
-  
-#  a ? b : c
-# if a then b else c end
-
-
-  end     
-    
-
-
-=begin
     doc.css("li div.ranking-item").each do |camp|
-
         name = camp.css("div.ranking-item__header--left h3 a").text
         rating = camp.css("div.ranking-item__header--left span.ranking-item__rating--value").text.split.join(' ')
         subjects = camp.css("table tr:nth-child(3) td").text.split.join(' ')
         url = camp.css("div.ranking-item__header h3 a").attribute("href").value 
         BestOnlineCodingBootcamps::Bootcamps.new(name, rating, subjects, url)
-        
     end
-=end
-=begin
+  end     
+    
   def self.scrape_details(bootcamps)
     html = open("https://www.switchup.org#{bootcamps.url}")
     doc = Nokogiri::HTML(html)
-#    binding.pry
-#      about = doc.css("h2.topic-title").text
-#      website =
-#      locations =
-#      programs =
-#      scholarships = 
-#      info =
+    
+    about = doc.css("h2.topic-title").text.split.join(' ')
+    website = doc.css("div.ranking-item a").attribute("href").value 
+        
+      if doc.css("div.span12 blockquote.topic-text p").text.include? "..." then info = doc.css("div.span12 blockquote.topic-text p a").attribute("onclick").value[35..-25].gsub("<\\/p>\\n\\n<p>", " ").gsub("\\n<br />", " ") elsif !doc.css("div.span12 blockquote.topic-text p").text.include? "..." then info = doc.css("div.span12 blockquote.topic-text p").text.chomp end
+        
+      if doc.css("div.extra-info p:nth-child(3) span").text.include? "..." then scholarships = doc.css("div.extra-info p:nth-child(3) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(3) span").text.include? "..." then scholarships = doc.css("div.extra-info p:nth-child(3) span").text.split.join(' ') end
+        
+      if doc.css("div.extra-info p:nth-child(2) span").text.include? "..." then programs = doc.css("div.extra-info p:nth-child(2) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(2) span").text.include? "..." then programs = doc.css("div.extra-info p:nth-child(2) span").text.split.join(' ') end
+        
+      if doc.css("div.extra-info p:nth-child(1) span").text.include? "..." then locations = doc.css("div.extra-info p:nth-child(1) span a").attribute("onclick").value[23..-18] elsif !doc.css("div.extra-info p:nth-child(1) span").text.include? "..." then locations = doc.css("div.extra-info p:nth-child(1) span").text.split.join(' ') end
   end
-=end  
 end  
