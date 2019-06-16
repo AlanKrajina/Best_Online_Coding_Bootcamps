173cb69b6c74d209829ee000d85a7c42c04a2e02
Alan Krajina <alankrajina88@gmail.com>
2019-06-16 09:42:21 +0000

    update

--- lib/Best_Online_Coding_Bootcamps/bootcamps.rb
+++ lib/Best_Online_Coding_Bootcamps/bootcamps.rb
@@ -2,39 +2,21 @@ require "pry"
 class BestOnlineCodingBootcamps::Bootcamps
     attr_accessor :name, :rating, :subjects, :url
 
-
-# default object creating class method
-  def self.create_from_collection(bootcamps)
-    bootcamps.each do |bootcamp|
-      Bootcamps.new(bootcamp)
-    end  
-  end
-
-
-  def scrape_and_create_camps
-    hash_of_camps = Scraper.scrape_index_page
-#    @bootcamp_object.create_from_collection(hash_of_camps)
-  end
-
-#    binding.pry
-  binding.pry  
-
-
-
-
-
-
-=begin
-
-#creates new_bootcamp object
-
+  @@all = []
   
-# hash?
-  def initialize(student_hash)
-    student_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
-    end
-    @@all << self
+  def initialize(name,rating,subjects,url)
+    @name = name
+    @rating = rating
+    @subjects = subjects
+    @url = url
+    save
   end
-=end
+
+ def save
+   @@all << self
+ end
+
+
+
+
 end
--- lib/Best_Online_Coding_Bootcamps/scraper.rb
+++ lib/Best_Online_Coding_Bootcamps/scraper.rb
@@ -6,34 +6,21 @@ class BestOnlineCodingBootcamps::Scraper
   def self.scrape_index_page
     html = open("https://www.switchup.org/rankings/best-online-bootcamps")
     doc = Nokogiri::HTML(html)
-    bootcamps = []
 
 
-  doc.css("li div.ranking-item").each do |bootcamp|
-      bootcamps << {
-        :name => bootcamp.css("div.ranking-item__header--left h3 a").text,
-        :rating => bootcamp.css("div.ranking-item__header--left span.ranking-item__rating--value").text.split.join(' '),
-        :subjects => bootcamp.css("table tr:nth-child(3) td").text.split.join(' '),
-        :url => bootcamp.css("div.ranking-item__header h3 a").attribute("href").value 
-      }
-  end
-#  binding.pry
-    bootcamps
+    doc.css("li div.ranking-item").each do |camp|
+        bootcamp = ForTheLoveOfDogs::Dogs.new
+  
+        bootcamp.name = camp.css("div.ranking-item__header--left h3 a").text
+        bootcamp.rating = camp.css("div.ranking-item__header--left span.ranking-item__rating--value").text.split.join(' ')
+        bootcamp.subjects = camp.css("table tr:nth-child(3) td").text.split.join(' ')
+        bootcamp.url = camp.css("div.ranking-item__header h3 a").attribute("href").value 
+        bootcamp.save
+    end
   end     
 
-#  def self.scrape_bootcamp_page(url)
-
- #   bootcamp_profile = {}
-    
-#    bootcamp_profile = "https://www.switchup.org/rankings/best-online-bootcamps" + url
-#    doc = Nokogiri::HTML(open(bootcamp_profile))
-
 
  
- 
-
-
-#  end
 
 
 end     
