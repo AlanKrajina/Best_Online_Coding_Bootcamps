075c0a7e407ecbef0c7ef280d15294933e833f6b
Alan Krajina <alankrajina88@gmail.com>
2019-06-17 11:41:02 +0000

    updated

--- lib/Best_Online_Coding_Bootcamps/cli.rb
+++ lib/Best_Online_Coding_Bootcamps/cli.rb
@@ -16,6 +16,8 @@ class BestOnlineCodingBootcamps::CLI
     
     
     puts "Type a number to select the bootcamp you want more info"
+    puts "2.LEVEL list of all bootcamps..."
+
     input = gets.strip
 
     bootcamp = BestOnlineCodingBootcamps::Bootcamps.find_all(input.to_i)
