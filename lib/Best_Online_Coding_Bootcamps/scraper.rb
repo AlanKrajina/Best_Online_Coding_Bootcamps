ba61824903658eeaeb7464b21777ee27b75df0c2
Alan Krajina <alankrajina88@gmail.com>
2019-06-17 18:47:39 +0000

    new

--- lib/Best_Online_Coding_Bootcamps/scraper.rb
+++ lib/Best_Online_Coding_Bootcamps/scraper.rb
@@ -21,17 +21,28 @@ class BestOnlineCodingBootcamps::Scraper
     html = open("https://www.switchup.org#{bootcamps.url}")
     doc = Nokogiri::HTML(html)
     
-    cnd1 = doc.css("div.span12 blockquote.topic-text p").text.include? "..."
+    cnd1 = doc.css("div.span12 blockquote.topic-text p").text.include? "..." 
     cnd2 = doc.css("div.extra-info p:nth-child(3) span").text.include? "..."
     cnd3 = doc.css("div.extra-info p:nth-child(2) span").text.include? "..."
     cnd4 = doc.css("div.extra-info p:nth-child(1) span").text.include? "..."
     bootcamps.about = doc.css("h2.topic-title").text.split.join(' ')
     bootcamps.website = doc.css("div.ranking-item a").attribute("href").value 
-        
-      if cnd1 then bootcamps.info = doc.css("div.span12 blockquote.topic-text p a").attribute("onclick").value[35..-25].gsub("<\\/p>\\n\\n<p>", " ").gsub("\\n<br />", " ").gsub("\\", " ") elsif !cnd1 then bootcamps.info = doc.css("div.span12 blockquote.topic-text p").text.chomp end
-        
-      if cnd3 then bootcamps.programs = doc.css("div.extra-info p:nth-child(2) span a").attribute("onclick").value[23..-18] elsif !cnd3 then bootcamps.programs = doc.css("div.extra-info p:nth-child(2) span").text.split.join(' ') end
-        
-      if cnd4 then bootcamps.locations = doc.css("div.extra-info p:nth-child(1) span a").attribute("onclick").value[23..-18] elsif !cnd4 then bootcamps.locations = doc.css("div.extra-info p:nth-child(1) span").text.split.join(' ') end
+    
+      if cnd3  
+        bootcamps.programs = doc.css("div.extra-info p:nth-child(2) span a").attribute("onclick").value[23..-18]
+        else 
+          bootcamps.programs = doc.css("div.extra-info p:nth-child(2) span").text.split.join(' ') end
+ 
+      if cnd4
+        bootcamps.locations = doc.css("div.extra-info p:nth-child(1) span a").attribute("onclick").value[23..-18]
+        else
+          bootcamps.locations = doc.css("div.extra-info p:nth-child(1) span").text.split.join(' ') end   
+      
+      if !cnd1 
+       bootcamps.info = doc.css("div.span12 blockquote.topic-text p").text.chomp 
+        elsif cnd1 && !doc.css("div.span12 blockquote.topic-text p a").attribute("href").value.include?("https")
+        bootcamps.info = doc.css("div.span12 blockquote.topic-text p a").attribute("onclick").value[35..-24].gsub("<\\/p>\\n\\n<p>", " ").gsub("\\n<br />", " ").gsub("\\", " ") 
+        else 
+          bootcamps.info = doc.css("div.span12 blockquote.topic-text p").text.chomp end
   end
 end  
